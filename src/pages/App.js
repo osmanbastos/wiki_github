b2fb01945a3e8be1f614d74990bacdffc6a14fbd
osmanbastos <osmanbastos@hotmail.com>
2023-06-20 23:41:58 -0300

    Github com React

---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import gitLogo from "../assets/github.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={gitLogo} width={72} height={72} />
+    </div>
+  );
+}
+
+export default App;
